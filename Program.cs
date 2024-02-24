bc3792cb928abe2e4347d03988c27deb3a58b161
RagendhuRamesh <ragendhuram@gmail.com>
2024-02-24 11:53:55 +0530

    Final Commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloApp15Feb;
+
+Console.WriteLine("Welcome to Auhtentication App");
+Console.WriteLine(SignIn.Auhtenticate("admin", "admin@1256"));
---
+++ SignIn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloApp15Feb
+{
+        public class SignIn
+        {
+            public static string Auhtenticate(string userName, string password)
+            {
+                string msg;
+                if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+                {
+                    msg = "Use name and password required";
+                }
+                else
+                if ((userName == "admin") && (password == "admin@1256"))
+                {
+                    msg = "Authentication Pass";
+                }
+                else
+                {
+                    msg = "Authentication Fail";
+                }
+                return msg;
+            }
+        }
+    }
